ed3878b3ab4d12ec71ece4412dc6783bb9456bcb
Cristoffer Ullholm <cristoffer.ullholm@pengvin.se>
2025-05-25 20:26:39 +0200

    WIP

---
+++ Domain/Reservation/UseCases/CreateReservationUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Error;
+
+namespace Domain.Reservation.UseCases
+{
+    internal class CreateReservationUseCase
+    {
+    }
+
+    public class Response
+    {
+        public class Fail(string message) : RoomAsyncError(message)
+        {
+            public class UnauthorizedAccess() : Fail(string.Empty);
+
+            public class InvalidReservationHolder() : Fail("Invalid reservation holder.");
+
+            public class InvalidRoom() : Fail("Room is not able to book or make a reservation for.");
+
+            public class InvalidDateRange() : Fail("Invalid date range for reservation. Start date must be before end date.");
+
+            public class RoomAlreadyReserved() : Fail("Room is already reserved for the given date range.");
+        }
+
+        public class Success
+        {
+            public Success(ReservationId reservationId) => ReservationId = reservationId;
+            public ReservationId ReservationId { get; set; }
+        }
+    }
+}
---
+++ Domain/ReservationHolder/ReservationHolderId.cs
@@ -0,0 +1,56 @@
+﻿namespace Domain.ReservationHolder
+{
+    public struct ReservationHolderId : IEquatable<ReservationHolderId>
+    {
+        string _value;
+
+        public ReservationHolderId(string value)
+        {
+            _value = value;
+        }
+
+        public readonly string Value => _value;
+        public readonly bool NoValue => string.IsNullOrEmpty(_value);
+        public readonly bool HasValue => !NoValue;
+
+        public override readonly int GetHashCode()
+            => _value.GetHashCode();
+
+        public readonly bool Equals(ReservationHolderId other) => Equals(other._value);
+        public readonly bool Equals(string? other) => string.Equals(_value, other, StringComparison.OrdinalIgnoreCase);
+
+        public override readonly bool Equals(object? obj)
+        {
+            if (obj is null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            if (obj is ReservationHolderId other)
+            {
+                return Equals(other);
+            }
+
+            if (obj is string stringValue)
+            {
+                return Equals(stringValue);
+            }
+
+            return false;
+        }
+
+        public override readonly string ToString() => _value;
+
+        public static implicit operator ReservationHolderId(string? value)
+            => new() { _value = value ?? string.Empty };
+
+        public static implicit operator ReservationHolderId(ReadOnlySpan<char> value)
+            => new() { _value = value.ToString() };
+
+        public static bool operator ==(ReservationHolderId left, string? right) => left.Equals(right);
+        public static bool operator ==(ReservationHolderId left, ReservationHolderId right) => left.Equals(right);
+
+        public static bool operator !=(ReservationHolderId left, string? right) => !(left == right);
+        public static bool operator !=(ReservationHolderId left, ReservationHolderId right) => !(left == right);
+    }
+}
