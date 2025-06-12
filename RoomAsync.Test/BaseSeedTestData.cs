0bdd10bb568d98cf75708113866c3759acc2dd8f
Cristoffer Ullholm <cristoffer.ullholm@pengvin.se>
2025-06-12 21:28:08 +0200

    Added UseCase test for Reservation

---
+++ RoomAsync.Test/BaseSeedTestData.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Infrastructure;
+using Domain.Infrastructure.Rooms;
+using Domain.Room;
+
+namespace RoomAsync.Test
+{
+    public class BaseSeedTestData : IAsyncDisposable
+    {
+        protected readonly AsyncServiceScope Scope;
+        protected readonly ApplicationDbContext DbContext;
+
+        protected BaseSeedTestData(TestFixture testFixture)
+        {
+            // Create a service scope
+            Scope = testFixture.CreateScopeAsync();
+
+            // Resolve the UserDriverPort
+            DbContext = Scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Scope.DisposeAsync();
+        }
+
+        public async Task SeedAvaibleRoomAsync(RoomType roomType, string name, int floorLevel, int number, CancellationToken cancellationToken = default)
+        {
+            // Ensure the database is created
+            await DbContext.Database.EnsureCreatedAsync(cancellationToken);
+            // Seed initial data if necessary
+            if (!DbContext.Rooms.Any())
+            {
+                DbContext.Rooms.AddRange(new List<RoomDataEntity>
+                {
+                    new RoomDataEntity { RoomId = $"{floorLevel}-{number}",  RoomType = roomType, FloorLevel = floorLevel, RoomName = name, RoomNumber = number, Status = RoomStatus.Available  }
+                });
+                await DbContext.SaveChangesAsync(cancellationToken);
+            }
+        }
+    }
+}
