409148980b1afdc95f10ce04cb8391f70beeb31f
Cristoffer Ullholm <cristoffer.ullholm@pengvin.se>
2025-08-03 00:59:13 +0200

    Fixade better authentication and api clients

---
+++ RoomAsync.Web/RoomAsync.Web.ApiService/Extensions/SessionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace RoomAsync.Web.ApiService.Extensions
+{
+
+    public static class SessionExtensions
+    {
+        public static void SetObject<T>(this ISession session, string key, T value)
+        {
+            var json = JsonSerializer.Serialize(value);
+            session.SetString(key, json);
+        }
+
+        public static T? GetObject<T>(this ISession session, string key)
+        {
+            var json = session.GetString(key);
+            return json is null ? default : JsonSerializer.Deserialize<T>(json);
+        }
+    }
+
+}
---
+++ RoomAsync.Web/RoomAsync.Web.Web/Login/UserContextService.cs
@@ -0,0 +1,39 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace RoomAsync.Web.Web.Login
+{
+
+    public class UserContextService
+    {
+        private readonly ProtectedSessionStorage _sessionStorage;
+
+        public UserContextService(ProtectedSessionStorage sessionStorage)
+        {
+            _sessionStorage = sessionStorage;
+        }
+
+        public UserContext? CurrentUser { get; set; }
+
+        public async Task<bool> IsLoggedIn()
+        {
+            if (CurrentUser != null)
+            {
+                return true;
+            }
+            if (_sessionStorage == null)
+            {
+                return false;
+            }
+            var result = await _sessionStorage.GetAsync<UserContext>("UserContext");
+            if (result.Success && result.Value != null)
+            {
+                CurrentUser = result.Value;
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+}
